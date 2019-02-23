4305ace89353f72107f6668c5c80209048353071
GIDEON <cheesyelmo@gmail.com>
2019-02-23 17:19:12 -0600

    fixed front end web app

---
+++ back-end/address.js
@@ -0,0 +1,52 @@
+const addresses = {
+    "Richard J. Daley Center": {
+        "id": "http://cover-your-case.com/courthouses/e257820a-de1a-4a25-b83d-330238cd5e7a",
+        "county": "Cook County",
+        "address": "50 W Washington St, Chicago, IL 60602",
+        "courtrooms": ["2401", "2609", "2010", "2404", "2110", "2304", "2309", "2103"]
+    },
+
+    "2nd Municipal District - Skokie": {
+        "id": "http://cover-your-case.com/courthouses/eaa3fb9f-22e0-43bb-8e82-932bc7a8892b",
+        "county": "Cook County",
+        "address": "5600 Old Orchard Road, Skokie, IL 60077",
+        "courtrooms": ["219", "106", "206", "101", "202"]
+    },
+    "3rd Municipal District - Rolling Meadows": {
+        "id": "http://cover-your-case.com/courthouses/ca23ff38-8284-45d5-9f88-891b6ecf4586",
+        "county": "Cook County",
+        "address": "2121 Euclid Ave, Rolling Meadows, IL 60008",
+        "courtrooms": ["204", "205L", "108", "106"]
+    },
+    "4th Municipal District - Maywood": {
+        "id": "http://cover-your-case.com/courthouses/17301ad6-379b-11e9-b210-d663bd873d93",
+        "county": "Cook County",
+        "address": "1500 Maybrook Dr # 235, Maywood, IL 60153",
+    },
+    "5th Municipal District - Bridgeview": {
+        "id": "http://cover-your-case.com/courthouses/85ae7480-379b-11e9-b210-d663bd873d93",
+        "county": "Cook County",
+        "address": "10220 S 76th Ave #205l, Bridgeview, IL 60455",
+    },
+    "Branch 23 & 29 - Chicago": {
+        "id": "http://cover-your-case.com/courthouses/e05c1220-379b-11e9-b210-d663bd873d93",
+        "county": "Cook County",
+        "address": "5555 West Grand Avenue, Chicago, IL 60639"
+    },
+    "Branch 35 & 38 - Chicago": {
+        "id": "http://cover-your-case.com/courthouses/4ab7736c-379c-11e9-b210-d663bd873d93",
+        "county": "Cook County",
+        "address": "727 East 111th Street, 1st Floor, Chicago, IL 60628"
+    },
+    "Branch 43 & 44 - Chicago": {
+        "id": "http://cover-your-case.com/courthouses/98f74c96-379c-11e9-b210-d663bd873d93",
+        "county": "Cook County",
+        "address": "3150 West Flournoy Street, 1st Floor, Chicago, IL 60612"
+    },
+    "Everett McKinley Dirksen United States Courthouse": {
+        "id": "http://cover-your-case.com/courthouses/d31bb8ca-379d-11e9-b210-d663bd873d93",
+        "county": "Cook County",
+        "address": "219 S Dearborn St, Chicago, IL 60604"
+    }
+}
+module.exports = addresses;
--- back-end/index.js
+++ back-end/index.js
@@ -2,10 +2,24 @@ const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
 const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 const twilioAuthToken = process.env.TWILIO_PRIMARY_TOKEN;
 
-const client = require('twilio')(twilioAccountSid, twilioAuthToken);
+const client = require('twilio');
+const proactiveClient = client(twilioAccountSid, twilioAuthToken);
+const MessagingResponse = client.twiml.MessagingResponse;
 
+const querystring = require('querystring');
+
+const address = require('./address');
 exports.handler = async (event, context) => {
-    return verifyRequest(event, context);
+    if (event.params && event.params.header && event["body-json"]
+        && event.params.header["User-Agent"] == "TwilioProxy/1.1") {
+        return incomingSms(event, context);
+    } else if (event.Date) {
+        console.log(JSON.stringify(event));
+        return verifyRequest(event, context);
+    } else {
+        console.log(event);
+        return 'You have failed this city!!';
+    }
 };
 function verifyRequest(event, context) {
     //This is where you would ensure that the request was legit, 
@@ -25,22 +39,34 @@ function sendCoverageDemo(event, request) {
 
     let body = 'This is Cover Your Case with a new request:\n\n';
 
+    body += 'Requesting Attorney: ' + 'Jeffery S. Harris, Esq' + '\n';
     body += 'Date: ' + event.Date + '\n';
     body += 'Time: ' + event.Time + '\n';
     body += 'Location: ' + event.Courtroom + ', '
         + event.Courthouse + ', ' + event.County + '\n';
-    body += 'Judge: ' + event['Judge Name'] + '\n';
+
+    //check to see if this exists
+    if (address[event.Courthouse]) {
+        body += 'Address: ' + address[event.Courthouse].address + '\n';
+    }
+
+    //optional input
+    if (event['Judge Name']) {
+        body += 'Judge: ' + event['Judge Name'] + '\n';
+    }
+
+    body += 'Case Name: ' + event['Case Name'] + '\n';
 
     //optional input but could be critical with picking up the case
-    if (event['Any Extra Comments']) {
+    if (event.Instructions) {
         body += 'Special Notes by the Requesting Attorney: ' +
             event['Any Extra Comments'] + '\n';
     }
 
     let obj = { from, to, body };
-    return client.messages.create(obj)
+    return proactiveClient.messages.create(obj)
         .then(d => {
-            console.log(JSON.stringify(d));
+            console.log(d);
             return 'We have received your request and this is your id: ' + request;
         })
         .catch(e => {
@@ -49,4 +75,50 @@ function sendCoverageDemo(event, request) {
                 + '.  Please contact Jeffrey S. Harris, Esq for more information';
         });
 
+}
+function incomingSms(event, context) {
+
+    let json = querystring.parse(event["body-json"]);
+    console.log(json);
+    let body = json.Body;
+    let text = '';
+
+    //Add signature check
+    //Add real NLP later
+    if (typeof body == 'string') {
+        let nBody = body.toLowerCase();
+        if (nBody == 'accept') {
+            text = 'Type YES if you have current Malpractice Insurance or '
+                + 'NO if you do not have anything currently.';
+        } else if (nBody == 'reject') {
+            text = 'Sorry to hear that, I hope we can find you a different case in the future';
+        } else if (nBody == 'yes') {
+            text = 'Glad to hear that here is the rest of the details of the case.\n\n';
+            text += 'Case Number: ' + 'abc1234567' + '\n';
+            text += 'Division: ' + 'Civil' + '\n';
+            text += 'Practice Area: ' + 'Collections' + '\n';
+            text += 'Attachments: '
+                + 'https://s3.amazonaws.com/griot-voice-assistant/daily-chinese-quiz/alexa-icons/large_icon.png'
+                + '\n';
+        } else if (nBody == 'no') {
+            text = 'I am sorry but we cannot give you this case at this time.';
+            text += '  But with 15 minutes you might be able to save 15% on your insurace';
+        } else {
+            text = 'I am sorry, I am not sure what you mean.';
+        }
+    } else {
+        text = 'I am sorry, I am not sure what you mean.';
+    }
+    return sendSms(text);
+
+}
+function sendSms(text) {
+    // const response = new MessagingResponse();
+    // response.message(text);
+    // return response.toString();
+
+    let str = '<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Message>'
+        + text
+        + '</Message></Response>';
+    return str;
 }
